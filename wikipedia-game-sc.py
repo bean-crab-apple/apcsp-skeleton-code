f6d5ce95a720c69b1683dbd191459730a2f29fe7
bean crab-apple <deano.capellari@gmail.com>
2023-10-30 19:21:36 +0000

    i may have finally fixed it

--- wikipedia-game-sc.py
+++ wikipedia-game-sc.py
@@ -1,9 +1,11 @@
-from queue import Queue
 # queues don't work! when things are alphabetically added to a queue, they work maybe for like the first 20 or so elements(maybe up to "Af..." on large pages)
 # however, the last element in the list("Z...") fails to be found when queue.get is being run
 # does the queue structure have an inherent size limit?
 # btw redirect pages also completely break the API, for example, 'Nina Tandon' links to 'Biomedical engineer' which redirects to 'Biomedical engineering' but
 # fetch_links(wiki_wiki.page('Biomedical engineer')) does not return a list containing 'Biomedical engineering'
+# update: it's not queues that are broken(although this now uses a standard list) however I did have to add a shortcut break which saves effectively a whole
+# level of time and generally makes python behave more nicely because it no longer has this massive queue to manage
+# also the links in dropdowns at the bottoms of pages and the links to wiki metapages in the noteboxes are also returned by page.links
 import wikipediaapi
 import time
 
@@ -27,15 +29,15 @@ def wikipedia_game_solver(start_page, target_page):
   
     # FINISH THE CODE HERE
     visited = []
-    queue = Queue()
+    queue = []
     path = []
     parent = {}
 
-    queue.put(start_page.title)
+    queue.append(start_page.title)
     visited.append(start_page.title)
 
-    while not queue.empty():
-        current_page_title = queue.get()
+    while len(queue) != 0:
+        current_page_title = queue.pop(0)
         fetch_links(wiki_wiki.page(current_page_title))
         if current_page_title == target_page.title:
             break
@@ -45,8 +47,12 @@ def wikipedia_game_solver(start_page, target_page):
 
         for link in next_level:
             if link not in visited:
-                queue.put(link)
+                queue.append(link)
                 parent[link] = current_page_title
+            if link == target_page.title:
+                queue = []
+                print("DONE!")
+                break
     
     child = target_page.title
 
@@ -62,7 +68,7 @@ def wikipedia_game_solver(start_page, target_page):
     return path
 
 # Example usage:
-start_page = wiki_wiki.page('Apollo 11')
-target_page = wiki_wiki.page('Zond program')
+start_page = wiki_wiki.page('Applied Magnetics Corporation')
+target_page = wiki_wiki.page('24 Hours of Lemons')
 path = wikipedia_game_solver(start_page, target_page)
 print("Shortest path:", path)
