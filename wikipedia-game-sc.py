255306f54902720655146dc7a720d68b5d8b1418
bean crab-apple <deano.capellari@gmail.com>
2023-11-01 18:36:13 +0000

    ohh nahhhhh he dead ahh hell

--- wikipedia-game-sc.py
+++ wikipedia-game-sc.py
@@ -7,10 +7,11 @@
 # update: it's not queues that are broken(although this now uses a standard list) however I did have to add a shortcut break which saves effectively a whole
 # level of time and generally makes python behave more nicely because it no longer has this massive queue to manage
 # also the links in dropdowns at the bottoms of pages and the links to wiki metapages in the noteboxes are also returned by page.links
+from queue import Queue
 import wikipediaapi
 import time
 
-user_agent = "hulloits.wikifinder (goofygoff.minecraft@gmail.com)"
+user_agent = "MsOrret'sWikipediaGame/1.0 (orret.deborah@pusd.us)"
 
 wiki_wiki = wikipediaapi.Wikipedia(user_agent, "en")
 
@@ -19,8 +20,8 @@ def fetch_links(page):
     links_list = []
     links = page.links
     for title in sorted(links.keys()):
-        links_list.append(title) 
-    print(links_list) 
+        links_list.append(title)
+        
     return links_list
 
 #IN CLASS: Finish the definition of the wikipedia_game_solver using a Breadth-First-Search Traversal
@@ -29,39 +30,6 @@ def wikipedia_game_solver(start_page, target_page):
     start_time = time.time()
   
     # FINISH THE CODE HERE
-    visited = []
-    queue = []
-    path = []
-    parent = {}
-
-    queue.append(start_page.title)
-    visited.append(start_page.title)
-
-    while len(queue) != 0:
-        current_page_title = queue.pop(0)
-        fetch_links(wiki_wiki.page(current_page_title))
-        if current_page_title == target_page.title:
-            break
-        visited.append(current_page_title)
-        current_page = wiki_wiki.page(current_page_title)
-        next_level = fetch_links(current_page)
-
-        for link in next_level:
-            if link not in visited:
-                queue.append(link)
-                parent[link] = current_page_title
-            if link == target_page.title:
-                queue = []
-                print("DONE!")
-                break
-    
-    child = target_page.title
-
-    while child != start_page.title:
-        path.append(child)
-        child = parent[child]
-    path.append(start_page.title)
-    path.reverse()
 
     end_time = time.time()
     print("This algorithm took", end_time-start_time, "seconds to run!")
@@ -69,7 +37,7 @@ def wikipedia_game_solver(start_page, target_page):
     return path
 
 # Example usage:
-start_page = wiki_wiki.page('Applied Magnetics Corporation')
-target_page = wiki_wiki.page('24 Hours of Lemons')
+start_page = wiki_wiki.page('Nina Tandon')
+target_page = wiki_wiki.page('Italian language')
 path = wikipedia_game_solver(start_page, target_page)
 print("Shortest path:", path)
